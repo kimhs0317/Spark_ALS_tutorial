314132cef717d539fbef2812655789328ebb9c84
김현식 <gkakqkqh1@naver.com>
2017-08-25 15:20:47 +0900

    json 파일 저장

--- src/TripAdvisor/filtering_data.scala
+++ src/TripAdvisor/filtering_data.scala
@@ -3,7 +3,7 @@ package TripAdvisor
 import org.apache.spark.sql._
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.explode
+import org.apache.spark.sql.functions._
 
 object filtering_data {
   
@@ -15,24 +15,24 @@ object filtering_data {
       .master("local[*]")
       .appName("Read Data")
       .getOrCreate()
-      
-//    spark.sparkContext.setLogLevel("ERROR")
-      
+    spark.sparkContext.setLogLevel("ERROR")
+    
     try {
       val start = System.currentTimeMillis()
       
-      save_file(spark)
-//      read_file(spark)
+      //17minutes..
+//      save_file(spark)
+      read_file(spark)
       
       val end = System.currentTimeMillis()
       println("runtime : "+(end-start)/1000.0)
-      
     } catch {
       case error:Exception => println(error)
     }
   }
   
   def save_file(spark:SparkSession): Unit = {
+    import spark.implicits._
     val HotelData = spark.read.json(base_json_path+"json/*.json")
     
     //HotelData view 생성 후 query로 필요한 데이터를 뽑아온다.
@@ -46,24 +46,15 @@ object filtering_data {
         "LATERAL VIEW explode(Reviews.Author) a AS Author "+
         "GROUP BY Author"
     )
-    val base_data = spark.sql(
-        "select "+
-          "Author, "+
-          "HotelInfo.HotelID as itemID, "+
-          "Rating "+
-        "from "+
-          "HotelData "+
-        "LATERAL VIEW posexplode(Reviews.Author) a AS a_num, Author "+
-        "LATERAL VIEW posexplode(Reviews.Ratings.Overall) o AS r_num, Rating "+
-        "where "+
-          "a_num = r_num"
-    )
-    val data = base_data.join(seq, Seq("Author")).select("userID","itemID","Rating")
+    val base_data = HotelData.select(explode($"Reviews").as("Reviews"), $"HotelInfo.HotelID".as("itemID"))
+      .withColumn("Author", $"Reviews.Author")
+      .withColumn("Rating", $"Reviews.Ratings.Overall")
+    val data = base_data.join(seq, Seq("Author")).select("userID", "itemID", "Rating")
     data.coalesce(1).write.json(base_json_path+"Colaborative Filtering BaseData")
   }
   
   def read_file(spark:SparkSession): Unit = {
-    val sample = spark.read.json(base_json_path+"test_result/*.json")
+    val sample = spark.read.json(base_json_path+"Colaborative Filtering BaseData/*.json")
     sample.printSchema()
     sample.show()
   }
